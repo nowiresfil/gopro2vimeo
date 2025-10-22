4602f65fa5d29964834b283fb1efef0f7b4d7b5b
Cory <ccall48@hotmail.com>
2025-10-22 14:51:43 +1100

    add assume yes to package installs

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 PACKAGE_LIST="apt-transport-https ca-certificates curl gnupg openssl"
 DOCKER_LIST="docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
 sudo apt update && sudo apt upgrade -y
-sudo apt install $PACKAGE_LIST
+sudo apt install $PACKAGE_LIST -y
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian trixie stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install $DOCKER_LIST
+sudo apt install $DOCKER_LIST -y
 # add pi user to docker group
 sudo usermod -aG docker $USER
 
